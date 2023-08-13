b4d44f4c1e1ce3e70088871819a4dac8a630b1dc
Shaurya Gupta <119103602+shauryagupta3@users.noreply.github.com>
2023-08-13 20:58:26 +0530

    try env vars

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -2,9 +2,9 @@ export const APIurl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoAPIOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": env.VITE_geoAPI_Key,
+    "X-RapidAPI-Key": context.env.VITE_geoAPI_Key,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
-export const openweatherKey = env.VITE_openWeather_Key;  
+export const openweatherKey = context.env.VITE_openWeather_Key;  
